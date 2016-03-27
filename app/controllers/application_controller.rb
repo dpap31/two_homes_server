8a13e43cf2b6714f2809830dc2a3da6568282f91
Drew Pappas <drewpappas@mac.com>
2016-03-27 00:08:49 -0700

    added authentication and current user methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < JSONAPI::ResourceController
+
+  def current_user
+    @current_user ||= current_resource_owner
+  end
+
+  def current_resource_owner
+    return unless valid_doorkeeper_token?
+    User.find(doorkeeper_token.resource_owner_id)
+  end
 end
 
--- app/controllers/v1/users_controller.rb
+++ app/controllers/v1/users_controller.rb
@@ -1,4 +1,10 @@
 module V1
   class UsersController < ApplicationController
+    prepend_before_action :swap_id_for_me, only: [:show]
+
+    def swap_id_for_me
+      return unless params[:id] == 'me'
+      params[:id] = current_user.try(:id)
+    end
   end
 end
